b25fc494664750dc335c7ccbf0618b418c6d4b11
victorffernandes <victorfernandes.nave@gmail.com>
2019-02-09 23:33:02 -0200

    Ajustando construtor do router para executar o loggable

--- src/router.js
+++ src/router.js
@@ -2,9 +2,14 @@ import glob from 'glob';
 import Base from './lib/Base';
 
 class Router extends Base {
+    constructor(){
+        super();
+    }
+    
     importRoutes() {
         const cwd = process.cwd();
-        glob("./api/modules/**/route.js", function (er, files) {
+
+        glob("./api/modules/**/route.js", (er, files) => {
             files.forEach((rout) => {
                 const path = cwd + rout.substring(1, rout.length);
                 this.log.debug(`Imported route specified in ${path}`)
