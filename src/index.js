a24ba869f1f486414df711a93751d87681fe75e0
victorffernandes <victorfernandes.nave@gmail.com>
2019-02-09 23:09:56 -0200

    Ajuste nos direcionamentos do pacote

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import server from './utils/server';
-import config from './utils/config';
-import ConnectionFactory from './utils/connectionFactory'; 
+import server from './server';
+import config from './config';
+import ConnectionFactory from './connectionFactory'; 
 
 module.exports = {
     server, 
